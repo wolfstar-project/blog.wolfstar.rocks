65509a8613aa2dd4fe728dd7db6b7d1c833be69e
RedStars071 <redtwoghost@gmail.com>
2025-10-03 16:39:00 +0200

    fix(footer): update copyright year to 2025

--- app/components/sections/app-footer.vue
+++ app/components/sections/app-footer.vue
@@ -2,7 +2,7 @@
 	<footer>
 		<div class="container mx-auto flex flex-col items-center px-5 py-8 sm:flex-row">
 			<div class="mt-4 text-sm opacity-70 sm:mt-0 sm:py-2">
-				© 2024 WolfStar Project —
+				© 2025 WolfStar Project —
 				<nuxt-link href="https://github.com/wolfstar-project" class="hover:underline">GitHub</nuxt-link>
 			</div>
 		</div>
